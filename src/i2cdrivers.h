4664fc50dd05cab0f6ee3e776b67debdb43c1c13
wicked-rainman <40294416+wicked-rainman@users.noreply.github.com>
2023-04-05 12:56:47 +0100

    Create i2cdrivers.h

---
+++ src/i2cdrivers.h
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+#include <WiFi.h>
+//#include "extDefines.h"
+extern void UdpWrite(WiFiUDP *, IPAddress, int, SemaphoreHandle_t, TickType_t, char *);
+void LoadI2cDrivers(int, int[127],WiFiUDP *, int, SemaphoreHandle_t,int, bool);
+extern void SHT30drv(void *);
+extern void BMP280drv(void *);
+//extern void SHT30drv(void *);
+//extern void BMP280drv(void *);
+//extern void SGP30drv(void *);
+//extern void VL53L0Xdrv(void *);
+#define MAXIMUM_TASK_COUNT 15
+#define TOGGLE_CORE core ? core=false:core=true
+extern int task_count;
+extern const char *mHostname;
+extern TaskHandle_t tasks[MAXIMUM_TASK_COUNT];
